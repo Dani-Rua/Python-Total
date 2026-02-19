8df10fc9f0804d738730477aa4557609291edfc6
Dani-Rua <danigodmontoya@gmail.com>
2026-02-19 16:37:58 -0500

    Inicio del día 5

---
+++ Dia 5/Ayuda.py
@@ -0,0 +1,8 @@
+dic = {'1': 100, '2': 500}
+# Miramos la documentación de Python
+a = dic.popitem() # Elimina un par del diccionario aleatoriamente
+print(a) # Elimina una tupla
+print(dic) # Miramos como queda la tupla
+
+
+
